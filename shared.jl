f94a1d4ddcffecaf78c33227a94fb7848d0e7ce2
heetbeet <sfstreicher@gmail.com>
2020-07-07 20:55:05 +0200

    bugfix problem number

--- shared.jl
+++ shared.jl
@@ -8,7 +8,7 @@ imp = pyimport("imp")
 euleranswers = imp.load_source("euleranswers", abspath(@__DIR__)*"/files/euleranswers.py")
 
 function submit_answer(answer; prob_num=nothing)
-    if euleranswers.test_answer(answer; prob_num=1)
+    if euleranswers.test_answer(answer; prob_num=prob_num)
         IJulia.display(IJulia.HTML("<font color=#0acc2d size=\"7\">&#x2713;</font>"*string(answer)))
     else
         IJulia.display(IJulia.HTML("<font color=#cc0a0a size=\"7\">&#x2717;</font>"*string(answer)))
